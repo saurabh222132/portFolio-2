d2acc2de7a21ac4717f06c184583d9f57e81f126
Saurabh <skv222132@gmail.com>
2023-03-31 03:00:39 +0530

    letest-2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>This is the Index file in Heading tag</h1>
   </React.StrictMode>
 );
